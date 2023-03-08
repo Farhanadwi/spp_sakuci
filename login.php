803cea86946a9f1d6a80e516a3f1c19d767a7d0f
Ridhoo0 <ramadhanridho1104@gmail.com>
2023-03-08 08:04:08 +0700

    komit pertama

---
+++ login.php
@@ -0,0 +1,35 @@
+
+
+
+
+
+<div class="container pt-5">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="card">
+                <div class="class-header">
+                    <center> 
+                   Login Petugas
+                            </center>
+                        <center><img src="image/logo.png" alt="">
+                            </center>
+                    
+                </div>
+                <div class="card-body">
+                    <form action="index.php?aksi=login" method="post">
+                        
+                        <div class="form-group">
+                            <input type="text" name="username" placeholder=Username class="form-control">
+                            </div>
+                       
+                        <div class="form-group">
+                            <input type="password" name="password" placeholder=Password  class="form-control">
+                        </div>
+                        <button name="login" class="btn btn-primary" id="loginB"> Login </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ loginsiswa.php
@@ -0,0 +1,30 @@
+<div class="container pt-5">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="card">
+                <div class="class-header">
+                    <center> 
+                   Login Siswa
+                            </center>
+                        <center><img src="image/logo.png" alt="">
+                            </center>
+                    
+                </div>
+                <div class="card-body">
+                    <form action="index.php?aksi=loginsiswa" method="post">
+                        
+                        <div class="form-group">
+                            <input type="text" name="nisn" placeholder=nisn class="form-control">
+                            </div>
+                       
+                        <div class="form-group">
+                            <input type="nis" name="nis" placeholder=nis class="form-control">
+                        </div>
+                        <button name="login" class="btn btn-primary" id="loginB"> Login </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
